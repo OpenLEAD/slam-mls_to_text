f818d4ed3e9f9ea0a843f49e9e455524c3688a54
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-01-28 11:40:17 -0200

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,20 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+#include <iostream>
+
+namespace mls_to_text
+{
+	class DummyClass
+	{
+		public: 
+			/**
+			* Print a welcome to stdout
+			* \return nothing
+			*/
+			void welcome();
+	};
+
+} // end namespace mls_to_text
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <mls_to_text/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+	mls_to_text::DummyClass dummyClass;
+	dummyClass.welcome();
+
+	return 0;
+}
